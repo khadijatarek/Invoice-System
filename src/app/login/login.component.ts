cd5156de775ac895b22aeb341bda1d0d5248aad0
khadijakhaled <85907583+khadijakhaled@users.noreply.github.com>
2023-05-02 14:58:54 +0300

    some changes in login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FirebaseService } from '../shared/firebase.service';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -8,24 +10,32 @@ import { FirebaseService } from '../shared/firebase.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent {
   myForm!: FormGroup;
+  showErrorMessage: boolean = false;
 
 
-  constructor( private formBuilder: FormBuilder, private db: FirebaseService,)  {}
-
-  ngOnInit() {
+  constructor( private formBuilder: FormBuilder, private db: FirebaseService,private router: Router)  {
     this.myForm = this.formBuilder.group({
-      email: ['',[Validators.required, Validators.email]],
+      nID: ['',[Validators.required, Validators.minLength(4)]],
       password: ['',[Validators.required, Validators.minLength(8)]],
     });
   }
 
+
   onSubmit(form: FormGroup) {
     console.log('Valid?', form.valid); // true or false
-    console.log('Email', form.value.email);
-  }
 
+    const nID = this.myForm.controls['nID'].value;
+    const password = this.myForm.controls['password'].value;
+    console.log(nID,password);
 
+    this.db.authenticate(nID, password).subscribe(nationalID => {
+      console.log('Data sent to backend API:', nationalID);
+      this.router.navigate(["/home"]);
+      this.showErrorMessage = false;
+    });
+    (error:any) => console.log(`error is : $(error)`);this.showErrorMessage = true;
+    }
 
 }
--- src/app/shared/firebase.service.ts
+++ src/app/shared/firebase.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { idToken } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -33,5 +34,40 @@ export class FirebaseService {
     return this.http.put('https://ui-project-d452e-default-rtdb.firebaseio.com/serviceProviders' +" " + ".json", data);
 
   }
+
+  private baseUrl = 'https://ui-project-d452e-default-rtdb.firebaseio.com/customer/5555';
+
+
+  authenticate(NationalID: string, password: string): Observable<string> {
+    return this.http.get(`https://ui-project-d452e-default-rtdb.firebaseio.com/customer/${NationalID}.json`).pipe(
+      map((response: any) => {
+        if (response && response.password === password) {
+          return NationalID;
+        } else {
+          throw new Error('Invalid national ID or password');
+        }
+      })
+    );
+  }
+
+ /* login (email: any, password: any): Observable<any>
+  {
+    const url = `${this.baseUrl}.json?orderBy="email"&equalTo=${email}&limitToFirst=1`;
+
+    return this.http.get<{ [NationalID: string]: any }>(url)
+      .pipe(
+        map(customers => {
+          const NationalID = Object.keys(customers)[0];
+          const customer = customers[NationalID];
+          if (customer && customer.password === password) {
+            console.log(NationalID);
+            return NationalID;
+          } else {
+            throw new Error('Invalid email or password');
+          }
+        })
+      );
+  }*/
+
 
 }
